b4f7ade40c43d1a86cf79d04571beafdca634956
rajithkonara <rajithkonara@gmail.com>
2020-10-15 01:17:09 +0530

    added the application class

---
+++ src/main/java/com/nasa/perseverance/PerseveranceApplication.java
@@ -0,0 +1,37 @@
+package com.nasa.perseverance;
+
+import com.nasa.perseverance.commands.Command;
+import com.nasa.perseverance.domain.MarsRover;
+import com.nasa.perseverance.domain.Plateau;
+import com.nasa.perseverance.exception.MarsRoverOutOfBoundException;
+import com.nasa.perseverance.exception.PerseveranceException;
+import com.nasa.perseverance.util.CommandLineUtil;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class PerseveranceApplication {
+
+    private static final String QUIT = "q";
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Plateau plateau = CommandLineUtil.plateauInputs(input.nextLine());
+        if (plateau.isInvalidCoordinates(plateau.getUpperBoundX(), plateau.getUpperBoundY())) {
+            throw new PerseveranceException("Plateau coordinates should be positive");
+        }
+
+        while (!input.hasNext(QUIT)) {
+            MarsRover roverPositionInput = CommandLineUtil.getRoverPositionInput(input.nextLine(), plateau);
+            if (plateau.isInvalidRoverLocation(roverPositionInput.getCoordinateX(),
+                    roverPositionInput.getCoordinateY())) {
+                throw new MarsRoverOutOfBoundException();
+            }
+            List<Command> commandsInput = CommandLineUtil.getCommandsInput(input.nextLine());
+            roverPositionInput.executeCommands(commandsInput);
+            System.out.println(roverPositionInput.getRoverPosition());
+        }
+        System.out.println("Shutting down .....");
+        System.exit(1);
+    }
+}
